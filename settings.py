b02c82f93fd68085e9a378aac8d8d3f72600a49c
figsis <c.figueroa@tinbergen.nl>
2022-07-07 14:03:16 +0200

    Initial commit

---
+++ settings.py
@@ -0,0 +1,53 @@
+from os import environ
+
+
+SESSION_CONFIGS = [
+dict(
+        name='Button',
+        app_sequence=['svo'],
+        num_demo_participants=2,
+		select_items= 'PRIMARY',
+		items_in_random_order= False,
+		scale= 0.03 ,
+		slider_init= 'LEFT',
+		random_payoff= 'RAND',
+		precision= 'INTEGERS',
+		doc =  '' ,),
+]
+
+
+
+
+# if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
+# in SESSION_CONFIGS, except those that explicitly override it.
+# the session config can be accessed from methods in your apps as self.session.config,
+# e.g. self.session.config['participation_fee']
+
+SESSION_CONFIG_DEFAULTS = dict(
+    real_world_currency_per_point=1.00, participation_fee=0.00, doc=""
+)
+
+PARTICIPANT_FIELDS = []
+SESSION_FIELDS = []
+
+# ISO-639 code
+# for example: de, fr, ja, ko, zh-hans
+LANGUAGE_CODE = 'en'
+
+# e.g. EUR, GBP, CNY, JPY
+REAL_WORLD_CURRENCY_CODE = 'EUR'
+USE_POINTS = False
+
+
+ADMIN_USERNAME = 'admin'
+# for security, best to set admin password in an environment variable
+ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
+
+DEMO_PAGE_INTRO_HTML = """
+Here are some oTree games.
+"""
+
+
+SECRET_KEY = '2463227021408'
+
+INSTALLED_APPS = ['otree','dana','dana_timed', 'the_button','dat','svo']
