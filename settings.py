bc3faefb9fde453d1b0be004e7f38953c04ac792
figsis <c.figueroa@tinbergen.nl>
2022-07-07 15:49:30 +0200

    procfil

--- settings.py
+++ settings.py
@@ -22,7 +22,7 @@ DATABASES = {
         'NAME': 'mydb',
         'USER': 'myuser',
         'PASSWORD': 'password',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+        'HOST': '',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
